b3956bc9a60da63457fd32ed6d79587d8a0ed0df
shneor rebinovitz <shneor.333git config --global user.email shneor.333@gmail.com>
2022-05-24 22:42:18 +0300

    add home-page

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,3 +1,26 @@
-export const HomePage = () => {
-    return <h1>hfduashfdsui</h1>
+import React from 'react';
+import { Link } from 'react-router-dom';
+import imgHero from '../assets/img/hero.png';
+
+export function HomePage() {
+    return (
+        <section className="homepage">
+            <div className="hero flex">
+
+                <div className="hero-content flex column">
+                    <div className="hero-content-wrapper">
+                        <h1>Skello helps teams move work forward.</h1>
+                        <p>
+                            Collaborate, manage projects, and reach new productivity peaks. From high rises to the home
+                            office, the way your team works is unique accomplish it all with Skello.
+                        </p>
+                        <Link to="/workspace"> Start Demo </Link>
+                    </div>
+                </div>
+                <div>
+                    <img className="hero-img" src={imgHero} alt="" />
+                </div>
+            </div>
+        </section>
+    )
 }
