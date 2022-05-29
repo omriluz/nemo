64c045f6f01a7ddc70e309a79ed23aa8652645b4
Yonatan Ben Zeev <yonatanbz6@gmail.com>
2022-05-29 10:33:23 +0300

    ...

---
+++ src/cmps/board-app/task/task-description.jsx
@@ -0,0 +1,15 @@
+import { GrTextAlignLeft } from "react-icons/gr";
+
+export const Description = () => {
+    return <section className="task-description flex column ">
+        <div className="title-container flex">
+            <GrTextAlignLeft /> <h3>Description</h3>
+        </div>
+        <textarea className="text-desc" >
+
+        </textarea>
+
+    </section>
+
+
+}
