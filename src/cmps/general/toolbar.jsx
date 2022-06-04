b25a36c60a0ec1b1bdfe6bf62efe64710399f029
Omri <omriluz1994@gmail.com>
2022-06-04 20:01:33 +0300

    toolbar start

--- src/cmps/general/toolbar.jsx
+++ src/cmps/general/toolbar.jsx
@@ -1,5 +1,6 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Menu } from "./menu";
+import {AiOutlineStar,AiFillStar} from "react-icons/ai"
 
 export const ToolBar = ({ board }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,23 +13,14 @@ export const ToolBar = ({ board }) => {
 
   return (
     <div className="toolbar">
-      {/* {isMenuOpen && (
-        <DynamicModalCmp
-          modalDetails={menuDetails.current}
-          modalTitle={'Menu'}
-          onCloseModal={onCloseMenu}
-          width={340}
-          activities={board.activities}
-          boardId={board.id}
-        />
-      )} */}
-
       <Menu isMenuOpen={isMenuOpen} onCloseMenu={onCloseMenu} board={board} activities={board.activities} />
-
       <div className="toolbar-left">
         {/* <input type="text"  /> */}
+        <div className="board-toolbar-title-container">
         <h1 className="board-toolbar-title">{board.title}</h1>
-        <button className="toolbar-btn">star</button>
+        </div>
+        {/* <button className="toolbar-btn">star</button> */}
+        <span className="toolbar-btn star-btn">{board.isStar ? <AiFillStar color={'gold'} size={17}/> :<AiOutlineStar size={17}/>}</span>
         <span className="toolbar-divider"></span>
         <div className="toolbar-members">
           <div style={{ backgroundColor: "red" }} className="user-avatar"></div>
@@ -48,60 +40,3 @@ export const ToolBar = ({ board }) => {
     </div>
   );
 };
-// import { TiStarFullOutline, TiStarOutline } from "react-icons/ti";
-// import avatar from "../../assets/svg/avatar.svg";
-// import { IoPersonAddOutline, IoFilter } from "react-icons/io5";
-// import { MdMoreHoriz } from "react-icons/md";
-// import { loadBoard, updateBoard } from "../../store/actions/board.action";
-// import { useDispatch } from "react-redux";
-
-// export const ToolBar = ({board}) => {
-
-//     const dispatch = useDispatch()
-
-//     const onToggleStar = () => {
-//         board.isStar = !board.isStar
-//         dispatch(updateBoard(board))
-//       }
-
-//     return (
-//     <div className="board-header">
-//       <div className="main-nav flex align-center space-between">
-//         <div className="nav-left flex">
-//           <h1 className="nav-btn header-title">{board.title}</h1>
-//           <div className="nav-left-action flex">
-//             <div className="nav-btn fav">
-//               <span className="star-icon">
-//                 {board.isStar ? (
-//                   <TiStarFullOutline
-//                     className="star-icon star"
-//                     onClick={onToggleStar}
-//                   />
-//                 ) : (
-//                   <TiStarOutline className="star-icon" onClick={onToggleStar} />
-//                 )}
-//               </span>
-//             </div>
-//             <div className="nav-member">
-//               <img className="member-avatar" src={avatar} />
-//             </div>
-//             <div className="nav-btn add-member">
-//               <IoPersonAddOutline />
-//             </div>
-//           </div>
-//         </div>
-//         <div className="nav-right flex">
-//           <span className="nav-btn filter">
-//             {" "}
-//             <IoFilter />
-//             <p>Filter</p>
-//           </span>
-//           <button className="nav-btn show-menu">
-//             <MdMoreHoriz />
-//             <p>Show Menu</p>
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
